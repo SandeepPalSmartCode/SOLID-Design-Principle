f4dd62629718e2b04a5ccbb67ec256bae512c120
Sandeep Pal <134257370+SandeepPalSmartCode@users.noreply.github.com>
2023-05-22 12:06:32 +0530

    Open-Closed-Principle-in-C#

---
+++ Without-using the-Open-Closed-Principle-example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Without_using_the_Open_Closed_Principle_example
+{
+    public class Invoice
+    {
+        public double GetInvoiceDiscount(double amount, InvoiceType invoiceType)
+        {
+            double finalAmount = 0;
+            if (invoiceType == InvoiceType.FinalInvoice)
+            {
+                finalAmount = amount - 100;
+            }
+            else if (invoiceType == InvoiceType.ProposedInvoice)
+            {
+                finalAmount = amount - 50;
+            }
+            return finalAmount;
+        }
+    }
+    public enum InvoiceType
+    {
+        FinalInvoice,
+        ProposedInvoice
+    };
+}
