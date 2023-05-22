39eb9b7541b1c83f686b2b4689033493acf7ceb0
Sandeep Pal <134257370+SandeepPalSmartCode@users.noreply.github.com>
2023-05-22 12:23:13 +0530

    SOLID_PRINCIPLES.ISP.cs

---
+++ SOLID_PRINCIPLES.ISP.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PRINCIPLES.ISP
+{
+
+    #region volition ISP
+    //volition ISP
+    public interface IPrinterTasks
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+        void Fax(string FaxContent);
+        void PrintDuplex(string PrintDuplexContent);
+    }
+    public class HPLaserJetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+        public void Fax(string FaxContent)
+        {
+            Console.WriteLine("Fax content");
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            Console.WriteLine("Print Duplex content");
+        }
+    }
+    class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+        public void Fax(string FaxContent)
+        {
+            throw new NotImplementedException();
+            //voiltion of ISP
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            throw new NotImplementedException();
+
+            //voiltion of ISP
+        }
+    }
+    #endregion
+
+    #region Follow ISP
+    public interface IPrinterTasksNew
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+    }
+    interface IFaxTasks
+    {
+        void Fax(string content);
+    }
+    interface IPrintDuplexTasks
+    {
+        void PrintDuplex(string content);
+    }
+    public class HPLaserJetPrinternew : IPrinterTasksNew, IFaxTasks, IPrintDuplexTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+        public void Fax(string FaxContent)
+        {
+            Console.WriteLine("Fax content");
+        }
+        public void PrintDuplex(string PrintDuplexContent)
+        {
+            Console.WriteLine("Print Duplex content");
+        }
+
+    }
+    class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine("Print Done");
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine("Scan content");
+        }
+    }
+    #endregion
+}
