ea4996cc9c55f4db0a751dd4b15eb4afecfaddd6
Sandeep Pal <134257370+SandeepPalSmartCode@users.noreply.github.com>
2023-05-22 12:16:35 +0530

    SOLID_PRINCIPLES.LSP

---
+++ SOLID_PRINCIPLES.LSP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_PRINCIPLES.LSP
+{
+   
+    public interface IFruit
+    {
+        string GetColor();
+    }
+    public class Apple : IFruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : IFruit
+    {
+        public string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
