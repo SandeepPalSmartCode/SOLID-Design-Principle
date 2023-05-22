b07d554177c463d399c681095027b165ca4a74a2
Sandeep Pal <134257370+SandeepPalSmartCode@users.noreply.github.com>
2023-05-22 12:28:52 +0530

    Dependency Inversion Principle.cs

---
+++ Dependency Inversion Principle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion_Principle
+{
+    #region Violition of DIP
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+    public class EmployeeDataAccessLogic
+    {
+        public Employee GetEmployeeDetails(int id)
+        {
+            //In real time get the employee details from database
+            //but here we have hard coded the employee details
+            Employee emp = new Employee()
+            {
+                ID = id,
+                Name = "Pranaya",
+                Department = "IT",
+                Salary = 10000
+            };
+            return emp;
+        }
+    }
+    public class DataAccessFactory
+    {
+        public static EmployeeDataAccessLogic GetEmployeeDataAccessObj()
+        {
+            return new EmployeeDataAccessLogic();
+        }
+    }
+    public class EmployeeBusinessLogic
+    {
+        EmployeeDataAccessLogic _EmployeeDataAccessLogic;
+        public EmployeeBusinessLogic()
+        {
+            _EmployeeDataAccessLogic = DataAccessFactory.GetEmployeeDataAccessObj();
+        }
+        public Employee GetEmployeeDetails(int id)
+        {
+            return _EmployeeDataAccessLogic.GetEmployeeDetails(id);
+        }
+    }
+
+    #endregion
+
+    public interface IEmployeeDataAccessLogic
+    {
+        Employee GetEmployeeDetails(int id);
+        //Any Other Employee Related Method Declarations
+    }
+
+    public class EmployeeDataAccessLogicnew : IEmployeeDataAccessLogic
+    {
+        public Employee GetEmployeeDetails(int id)
+        {
+            //In real time get the employee details from database
+            //but here we have hard coded the employee details
+            Employee emp = new Employee()
+            {
+                ID = id,
+                Name = "Pranaya",
+                Department = "IT",
+                Salary = 10000
+            };
+            return emp;
+        }
+    }
+    public class DataAccessFactoryNew
+    {
+        public static IEmployeeDataAccessLogic GetEmployeeDataAccessObj()
+        {
+            return new EmployeeDataAccessLogicnew();
+        }
+    }
+}
